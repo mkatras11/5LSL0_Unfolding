85becb3d0e21cc6778c03fee3ff3539071d85977
Michael Katras <m.m.katras@student.tue.nl>
2022-06-19 14:43:54 +0200

    add pkspace function

--- week_2.py
+++ week_2.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # define parameters
 data_loc = os.path.dirname(os.path.realpath(__file__)) #change the datalocation to something that works for you
 data_loc = os.path.join(data_loc,'Fast_MRI_Knee')
-batch_size = 3
+batch_size = 2
 
 # create dataloaders
 train_loader, test_loader = create_dataloaders(data_loc, batch_size)
@@ -25,33 +25,43 @@ def image_to_kspace(image):
     kspace = torch.fft.fftshift(torch.fft.fft2(image, norm="forward"))
     return kspace
 
+# Create function to calculate partial kspace of MRI images
+def image_to_partialkspace(kspace, mask):
+    partialkspace = mask * kspace
+    return partialkspace
+
+
 kspce = image_to_kspace(gt)
+pkspce = image_to_partialkspace(kspce, M)
+
+pkspace_plot_friendly = torch.log(torch.abs(pkspce)+1e-20)
 kspace_plot_friendly = torch.log(torch.abs(kspce)+1e-20)
 
-
+vmin = torch.min(kspace_plot_friendly)
+vmax = torch.max(kspace_plot_friendly)
 
 plt.figure(figsize = (10,10))
 plt.subplot(1,4,1)
-plt.imshow(kspace_plot_friendly[0,:,:], interpolation='nearest')
-plt.xticks([])
-plt.yticks([])
-plt.title('k-space')
-
-plt.subplot(1,4,2)
 plt.imshow(gt[0,:,:],cmap='gray')
 plt.xticks([])
 plt.yticks([])
 plt.title('ground truth')
 
-plt.subplot(1,4,3)
-plt.imshow(kspace_plot_friendly[1,:,:], interpolation='nearest')
+plt.subplot(1,4,2)
+plt.imshow(kspace_plot_friendly[0,:,:],vmin=vmin, vmax=vmax, interpolation='nearest')
 plt.xticks([])
 plt.yticks([])
 plt.title('k-space')
 
-plt.subplot(1,4,4)
-plt.imshow(gt[1,:,:],cmap='gray')
+plt.subplot(1,4,3)
+plt.imshow(M[0,:,:], interpolation='nearest')
 plt.xticks([])
 plt.yticks([])
-plt.title('ground truth')
+plt.title('measurement mask')
+
+plt.subplot(1,4,4)
+plt.imshow(pkspace_plot_friendly[0,:,:],vmin=vmin, vmax=vmax, interpolation='nearest')
+plt.xticks([])
+plt.yticks([])
+plt.title('partial k-space')
 plt.show()
