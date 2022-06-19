cd98567d4e3b9286819e171fd97c798da806e75b
Michael Katras <m.m.katras@student.tue.nl>
2022-06-19 14:10:34 +0200

    Create image to kspace function

--- week_2.py
+++ week_2.py
@@ -3,16 +3,55 @@ from Fast_MRI_dataloader import create_dataloaders
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import tqdm
+import matplotlib.pyplot as plt
 
 # define parameters
 data_loc = os.path.dirname(os.path.realpath(__file__)) #change the datalocation to something that works for you
 data_loc = os.path.join(data_loc,'Fast_MRI_Knee')
-batch_size = 2
+batch_size = 3
 
 # create dataloaders
 train_loader, test_loader = create_dataloaders(data_loc, batch_size)
 
 # go over the dataset
-for i,(kspace, M, gt) in enumerate(tqdm(test_loader)):
+gt = []
+for i,(kspace, M, gt) in enumerate(test_loader):
     continue
 
+
+
+# Create function to calculate the K-space of MRI images
+def image_to_kspace(image):
+    kspace = torch.fft.fftshift(torch.fft.fft2(image, norm="forward"))
+    return kspace
+
+kspce = image_to_kspace(gt)
+kspace_plot_friendly = torch.log(torch.abs(kspce)+1e-20)
+
+
+
+plt.figure(figsize = (10,10))
+plt.subplot(1,4,1)
+plt.imshow(kspace_plot_friendly[0,:,:], interpolation='nearest')
+plt.xticks([])
+plt.yticks([])
+plt.title('k-space')
+
+plt.subplot(1,4,2)
+plt.imshow(gt[0,:,:],cmap='gray')
+plt.xticks([])
+plt.yticks([])
+plt.title('ground truth')
+
+plt.subplot(1,4,3)
+plt.imshow(kspace_plot_friendly[1,:,:], interpolation='nearest')
+plt.xticks([])
+plt.yticks([])
+plt.title('k-space')
+
+plt.subplot(1,4,4)
+plt.imshow(gt[1,:,:],cmap='gray')
+plt.xticks([])
+plt.yticks([])
+plt.title('ground truth')
+plt.show()
